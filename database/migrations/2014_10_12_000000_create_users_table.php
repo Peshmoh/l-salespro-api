084867b79b7d068873f2fcb1c150002d9557a214
pesh moh <perismumbi96@gmail.com>
2025-06-27 10:10:30 +0300

    added role column in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('Sales Representative'); // Added role column
+            $table->json('permissions')->nullable(); // Added permissions column    
             $table->rememberToken();
             $table->timestamps();
         });
